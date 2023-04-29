7b391c602a5b6eb1f6df39e8b9abee4c04d9d6fa
Eol1881 <02f1881@gmail.com>
2023-04-29 03:37:56 +0300

    feat: add background image, adjust some styles (big style changes)

--- virtual-keyboard/src/js-modules/physical_keyboard.js
+++ virtual-keyboard/src/js-modules/physical_keyboard.js
@@ -1,29 +1,82 @@
-// import { KEYBOARD } from "./basic_layout";
-// import { keyFunctions } from "./key_functions";
-// import { TEXTAREA } from "./basic_layout";
+import { KEYBOARD } from "./basic_layout";
 
-// const KEYS_ARR = KEYBOARD.querySelectorAll('.keyboard__key');
+const radioWrapper = document.createElement('div');
+radioWrapper.classList.add('radio-wrapper');
 
-// window.addEventListener('keydown', function(event) {
-//   event.preventDefault();
-//   console.log(event.code);
+class radioInput {
+  constructor(name, value, checked) {
+    this.input = document.createElement('input');
+    this.input.classList.add('radio-wrapper__radio');
+    this.input.type = 'radio';
+    this.input.name = name;
+    this.input.value = value;
+    this.input.checked = checked;
+		return this.input;
+  }
+}
+
+const radioOption1 = new radioInput("option", "option1", true);
+const radioOption2 = new radioInput("option", "option2", false);
+const radioOption3 = new radioInput("option", "option3", false);
+const radioOption4 = new radioInput("option", "option4", false);
+console.log(radioOption1);
+
+radioWrapper.append(radioOption1, radioOption2, radioOption3, radioOption4);
+KEYBOARD.insertAdjacentElement('afterend', radioWrapper);
+
+radioWrapper.addEventListener('change', (event) => {
+  const clickedValue = event.target.value;
+  if (clickedValue === 'option1') {
+		KEYBOARD.classList.remove('keyboard-background-1');
+		KEYBOARD.classList.remove('keyboard-background-2');
+		KEYBOARD.classList.remove('keyboard-background-3');
+  } else if (clickedValue === 'option2') {
+    KEYBOARD.classList.add('keyboard-background-1');
+		KEYBOARD.classList.remove('keyboard-background-2');
+		KEYBOARD.classList.remove('keyboard-background-3');
+  } else if (clickedValue === 'option3') {
+		KEYBOARD.classList.remove('keyboard-background-1');
+		KEYBOARD.classList.add('keyboard-background-2');
+		KEYBOARD.classList.remove('keyboard-background-3');
+  } else if (clickedValue === 'option4') {
+    KEYBOARD.classList.remove('keyboard-background-1');
+		KEYBOARD.classList.remove('keyboard-background-2');
+		KEYBOARD.classList.add('keyboard-background-3');
+  }
+});
 
 
+// radio1.classList.add('radio-wrapper__radio', 'radio-wrapper__radio--1');
+// radio1.type = "radio";
+// radio1.name = "option";
+// radio1.value = "option1";
+// radio1.checked = true;
 
-//   const keyDatasetValue = event.code;
-//   console.log('~~~~~', keyDatasetValue);
+// const radio2 = document.createElement("input");
+// radio2.classList.add('radio-wrapper__radio', 'radio-wrapper__radio--2');
+// radio2.type = "radio";
+// radio2.name = "option";
+// radio2.value = "option2";
 
-//   if (keyFunctions[keyDatasetValue]) {
-//     //console.log(e);
-//     cursorPosition = keyFunctions[keyDatasetValue](TEXTAREA, cursorPosition);
-//     updateCursorPosition();
-//   } else {
-//     TEXTAREA.value = TEXTAREA.value.slice(0, cursorPosition) + e.target.textContent + TEXTAREA.value.slice(cursorPosition);
-//     cursorPosition += 1;
-//     updateCursorPosition();
+// const radio3 = document.createElement("input");
+// radio3.classList.add('radio-wrapper__radio', 'radio-wrapper__radio--3');
+// radio3.type = "radio";
+// radio3.name = "option";
+// radio3.value = "option3";
+
+// radioWrapper.append(radio1, radio2, radio3);
+// KEYBOARD.insertAdjacentElement('afterend', radioWrapper);
+
+// radioWrapper.addEventListener('change', (event) => {
+//   const clickedValue = event.target.value;
+//   if (clickedValue === 'option1') {
+//     KEYBOARD.classList.remove('keyboard-background-1');
+// 		KEYBOARD.classList.remove('keyboard-background-2');
+//   } else if (clickedValue === 'option2') {
+//     KEYBOARD.classList.add('keyboard-background-1');
+// 		KEYBOARD.classList.remove('keyboard-background-2');
+//   } else if (clickedValue === 'option3') {
+//     KEYBOARD.classList.add('keyboard-background-2');
+// 		KEYBOARD.classList.remove('keyboard-background-1');
 //   }
 // });
-
-
-
-
